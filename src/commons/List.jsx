b2d2b7b8d962db6d00eccd08422e84569008e293
davidrorez <126507534+davidrorez@users.noreply.github.com>
2023-07-08 02:00:53 -0600

    Update List.jsx

--- src/commons/List.jsx
+++ src/commons/List.jsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import Table from 'react-bootstrap/Table';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import '../App.css';
+import { putFetch } from './ApiMethods';
 
 const List = ({ contents }) => {
   const [orders, setOrders] = useState([]);
